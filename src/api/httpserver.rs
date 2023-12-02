0b65fc608d9ce8d444d1803df08b22d5c901de63
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-12-02 14:48:26 +0200

    Add shorten and redirect endpoints.

--- src/api/httpserver.rs
+++ src/api/httpserver.rs
@@ -1,12 +1,16 @@
 use crate::configuration::settings::Settings;
-use crate::constants::APPLICATION_JSON;
+use crate::constants::{APPLICATION_JSON, TEXT_HTML};
 
-use actix_web::{middleware, App, HttpResponse};
+use actix_web::{middleware, App, HttpResponse, web};
 use actix_web::HttpServer;
-
 use std::io;
 
 use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Deserialize)]
+pub struct ShortenRequest {
+   long_url: String
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
@@ -20,6 +24,8 @@ pub async fn start_http_server(settings: &Settings) -> io::Result<()> {
             .wrap(middleware::Logger::default())
             // register HTTP requests handlers
             .service(hello)
+            .service(shorten)
+            .service(redirect)
     })
     .bind(&settings.apiserver.application_url)?
     .run()
@@ -31,4 +37,22 @@ async fn hello() -> HttpResponse {
 HttpResponse::Ok()
     .content_type(APPLICATION_JSON)
     .json(Response { message: String::from("hello")})
+}
+
+#[get("/shorten")]
+async fn shorten(info: web::Query<ShortenRequest>) -> actix_web::Result<String> {
+    dbg!(&info.long_url);
+    let hostname = "localhost:8000";
+    let hash = "zn9edcu";
+    Ok(format!("{}/{}", hostname, hash))
+}
+
+#[get("/{short_url}")]
+async fn redirect(path: web::Path<String>) -> HttpResponse {
+    let short_url = path.into_inner();
+    dbg!(short_url);
+    HttpResponse::PermanentRedirect()
+        .append_header(("location", "https://docs.rs/actix-web/latest/actix_web/"))
+        .content_type(TEXT_HTML)
+        .finish()
 }
--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,2 @@
 pub const APPLICATION_JSON: &str = "application/json";
+pub const TEXT_HTML: &str = "text/html";
