dad89f3af295ea3551c34f94a694d211df71f8df
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-12-25 17:01:41 +0200

    Implement Firestore hash service.

--- src/services/firestorehashservice.rs
+++ src/services/firestorehashservice.rs
@@ -2,9 +2,10 @@ use crate::{services::hashservice, models::{linkinfo::LinkInfo, queryparams::Que
 
 use firestore::*;
 use async_trait::async_trait;
-use redis::{Commands, JsonCommands};
 
 use super::{hashfunction, hashserviceerror::HashServiceError};
+
+const COLLECTION_NAME: &'static str = "links";
 
 pub struct FirestoreHashService {
     firestore_config: configuration::settings::FirestoreConfig,
@@ -28,23 +29,75 @@ impl hashservice::HashService for FirestoreHashService {
         Ok(())
     }
 
-    async fn get_links(&mut self, query_info: Option<QueryParams>) -> Result<Vec<LinkInfo>, HashServiceError> {
-        // Implement the logic for get_links
-        unimplemented!()
+    async fn get_links(&mut self, query_params: Option<QueryParams>) -> Result<Vec<LinkInfo>, HashServiceError> {
+        let urls : Vec<LinkInfo> = self.db.as_mut().unwrap().fluent()
+            .select()
+            .from(COLLECTION_NAME)
+            .obj()
+            .query()
+            .await?;
+
+        let query_params = match query_params {
+            Some(value) => value,
+            None => return Ok(urls)
+        };
+
+        let top = query_params.top.unwrap_or(urls.len());
+        let skip = query_params.skip.unwrap_or(0);
+        
+        Ok(urls.into_iter().skip(skip).take(top).collect())
     }
 
     async fn insert(&mut self, value: &str) -> Result<LinkInfo, HashServiceError> {
-        // Implement the logic for insert
-        unimplemented!()
+        let hash_value = hashfunction::hash(value);
+
+        let find_result = self.find(&hash_value).await?;
+        if find_result.is_some() {
+            return Ok(find_result.unwrap());
+        }
+        
+        let new_link = LinkInfo{
+            short_url: hash_value.clone(),
+            long_url: String::from(value),
+            clicks: 0
+        };
+
+        self.db.as_mut().unwrap().fluent()
+            .insert()
+            .into(COLLECTION_NAME)
+            .document_id(&hash_value)
+            .object(&new_link)
+            .execute::<()>()
+            .await?;
+        Ok(new_link)
     }
 
     async fn find(&mut self, key: &str) -> Result<Option<LinkInfo>, HashServiceError> {
-        // Implement the logic for find
-        unimplemented!()
+        let find_result: Option<LinkInfo> = self.db.as_mut().unwrap().fluent()
+            .select()
+            .by_id_in(COLLECTION_NAME)
+            .obj()
+            .one(&key)
+            .await?;
+
+        let mut unwrapped_result = match find_result {
+            Some(value) => value,
+            None => return Ok(None),
+        };
+        
+        unwrapped_result.clicks += 1;
+        return Ok(Some(unwrapped_result))
     }
 
     async fn delete(&mut self, key: &str) -> Result<bool, HashServiceError> {
-        // Implement the logic for delete
-        unimplemented!()
+        self.db.as_mut().unwrap().fluent()
+            .delete()
+            .from(COLLECTION_NAME)
+            .document_id(&key)
+            .execute()
+            .await?;
+
+        // TODO: Check if the document was deleted
+        return Ok(true)
     }
 }
