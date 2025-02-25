5da88bd473cf175d32c6021e9136048763cf1ad0
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-02-25 10:02:20 +0200

    Extract collector's consts into shared file.

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,11 @@
 pub const APPLICATION_JSON: &str = "application/json";
 pub const TEXT_HTML: &str = "text/html";
+pub const USER_AGENT_REGEX: &str = "regexes.yaml";
+pub const REMOTE_ADDR: &str = "Remote-Addr";
 
 pub const DEFAULT_CAPACITY: usize = 10;
 pub const DEFAULT_FILL_RATE: usize = 2;
+
+pub fn get_ip_url(ip: String) -> String {
+    return format!("http://ip-api.com/json/{}", ip);
+}
--- src/stats/collector.rs
+++ src/stats/collector.rs
@@ -1,8 +1,8 @@
-use actix_web::http::header::HeaderMap;
+use actix_web::http::header::{self, HeaderMap};
 use user_agent_parser::UserAgentParser;
 use serde_json::Value;
 
-use crate::models::analytic::{self, Analytic};
+use crate::{constants, models::analytic::{self, Analytic}};
 
 pub async fn collect_stats(headers: &HeaderMap) -> Analytic {
     let language = extract_language(&headers);
@@ -26,15 +26,15 @@ pub async fn collect_stats(headers: &HeaderMap) -> Analytic {
 
 fn extract_language(headers: &HeaderMap) -> Option<String> {
     headers
-        .get("Accept-Language")
+        .get(header::ACCEPT_LANGUAGE.as_str())
         .and_then(|lang| lang.to_str().ok())
         .map(|s| s.split(',').next().unwrap_or("").to_string())
 }
 
 fn extract_os(headers: &HeaderMap) -> Option<String> {
-    if let Some(user_agent) = headers.get("User-Agent") {
+    if let Some(user_agent) = headers.get(header::USER_AGENT.as_str()) {
         if let Ok(ua_str) = user_agent.to_str() {
-            let ua_parser = match UserAgentParser::from_path("regexes.yaml") {
+            let ua_parser = match UserAgentParser::from_path(constants::USER_AGENT_REGEX) {
                 Ok(parser) => parser,
                 Err(_) => return None
             };
@@ -46,14 +46,14 @@ fn extract_os(headers: &HeaderMap) -> Option<String> {
 
 fn extract_ip(headers: &HeaderMap) -> Option<String> {
     headers
-        .get("X-Forwarded-For") // Check if behind a proxy
+        .get(header::X_FORWARDED_FOR.as_str()) // Check if behind a proxy
         .and_then(|ip| ip.to_str().ok())
-        .or_else(|| headers.get("Remote-Addr").and_then(|ip| ip.to_str().ok()))
+        .or_else(|| headers.get(constants::REMOTE_ADDR).and_then(|ip| ip.to_str().ok()))
         .map(|s| s.split(',').next().unwrap_or("").to_string())
 }
 
 async fn extract_geolocation(ip: &str) -> Option<String> {
-    let url = format!("http://ip-api.com/json/{}", ip);
+    let url = constants::get_ip_url(ip.to_string());
     if let Ok(response) = reqwest::get(&url).await {
         if let Ok(json) = response.json::<Value>().await {
             if let (Some(country), Some(city)) = (json["country"].as_str(), json["city"].as_str()) {
